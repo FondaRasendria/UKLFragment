334d98d66eb55e66db702195d7b800ea2b51c968
FondaRasendria <fonfarasendria@gmail.com>
2023-02-21 21:08:56 +0700

    commit

--- app/src/main/java/com/example/ukl/MainActivity2.kt
+++ app/src/main/java/com/example/ukl/MainActivity2.kt
@@ -3,19 +3,29 @@ package com.example.ukl
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity2 : AppCompatActivity() {
-    lateinit var text: TextView
+    lateinit var textDikirim: TextView
+    lateinit var recycler: RecyclerView
+    lateinit var adapter: Adapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
 
-        text = findViewById(R.id.emailText)
+        textDikirim = findViewById(R.id.dikirim)
+        recycler = findViewById(R.id.recycler)
 
         val email = intent.getStringExtra("String")
-        if(email != null){
-            text.text = email
-        }
+
+        recycler.layoutManager = LinearLayoutManager(this)
+
+        var data = ArrayList<Item>()
+        data.addAll(ItemData().listData)
+
+        adapter = Adapter(data)
+        recycler.adapter = adapter
     }
 }
